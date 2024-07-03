9a7755800c834e6bd120ee58a4a504c99d0a467c
Maria Daphine <daphinewanyama@gmail.com>
2024-07-03 22:50:12 +0300

    residgned target page

--- lib/pages/targetpage.dart
+++ lib/pages/targetpage.dart
@@ -6,57 +6,140 @@ class Targetpage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
         backgroundColor: Colors.blue[200],
         centerTitle: true,
-        title:  const Text(
+        title: const Text(
           'WHAT WOULD YOU WANT TO SAVE FOR?',
           style: TextStyle(
             fontWeight: FontWeight.bold,
-            color:Colors.black,
-            fontFamily: 'Anton',
-
+            fontFamily: 'Poppins',
           ),
-
         ),
-
       ),
-      body: Column(
-        children: <Widget>[
-          Container(
-            color: Colors.blueGrey,
-            child: const Text(
-          
-              'Phone',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 20.0,
-                fontFamily: 'Anton',
-          
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              padding: const EdgeInsets.all(20.0),
+              decoration: BoxDecoration(
+                color: Colors.blueGrey,
+                borderRadius: BorderRadius.circular(10.0),
+                boxShadow: const [
+                  BoxShadow(
+                    color: Colors.black26,
+                    offset: Offset(0, 4),
+                    blurRadius: 5.0,
+                  ),
+                ],
               ),
-
-
-            ),
-          ),
-          Container(
-            color: Colors.blueGrey,
-            child:  const Text(
-              'Shopping',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 20.0,
-                fontFamily: 'Anton',
-
-              ),
+              child: const Text(
+                'Gadgets',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontFamily: 'Poppins',
+                  color: Colors.white,
+                ),
               ),
             ),
-
-          
-
-        ],
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              padding: const EdgeInsets.all(20.0),
+              decoration: BoxDecoration(
+                color: Colors.blueGrey,
+                borderRadius: BorderRadius.circular(10.0),
+                boxShadow: const [
+                  BoxShadow(
+                    color: Colors.black26,
+                    offset: Offset(0, 4),
+                    blurRadius: 5.0,
+                  ),
+                ],
+              ),
+              child: const Text(
+                'Shopping',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontFamily: 'Poppins',
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              padding: const EdgeInsets.all(20.0),
+              decoration: BoxDecoration(
+                color: Colors.blueGrey,
+                borderRadius: BorderRadius.circular(10.0),
+                boxShadow: const [
+                  BoxShadow(
+                    color: Colors.black26,
+                    offset: Offset(0, 4),
+                    blurRadius: 5.0,
+                  ),
+                ],
+              ),
+              child: const Text(
+                'Tuiton',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontFamily: 'Poppins',
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              padding: const EdgeInsets.all(20.0),
+              decoration: BoxDecoration(
+                color: Colors.blueGrey,
+                borderRadius: BorderRadius.circular(10.0),
+                boxShadow: const [
+                  BoxShadow(
+                    color: Colors.black26,
+                    offset: Offset(0, 4),
+                    blurRadius: 5.0,
+                  ),
+                ],
+              ),
+              child: const Text(
+                'Starting A Business',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontFamily: 'Poppins',
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              padding: const EdgeInsets.all(20.0),
+              decoration: BoxDecoration(
+                color: Colors.blueGrey,
+                borderRadius: BorderRadius.circular(10.0),
+                boxShadow: const [
+                  BoxShadow(
+                    color: Colors.black26,
+                    offset: Offset(0, 4),
+                    blurRadius: 5.0,
+                  ),
+                ],
+              ),
+              child: const Text(
+                'Others',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontFamily: 'Poppins',
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
-
     );
   }
 }
