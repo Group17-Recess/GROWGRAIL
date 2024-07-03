1681dc6d34901e0eb2819552ae4996321d197a4a
Maria Daphine <daphinewanyama@gmail.com>
2024-07-03 23:06:40 +0300

    added buttons

--- lib/pages/targetpage.dart
+++ lib/pages/targetpage.dart
@@ -22,123 +22,98 @@ class Targetpage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Container(
-              margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-              padding: const EdgeInsets.all(20.0),
-              decoration: BoxDecoration(
-                color: Colors.blueGrey,
-                borderRadius: BorderRadius.circular(10.0),
-                boxShadow: const [
-                  BoxShadow(
-                    color: Colors.black26,
-                    offset: Offset(0, 4),
-                    blurRadius: 5.0,
-                  ),
-                ],
-              ),
-              child: const Text(
-                'Gadgets',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  fontFamily: 'Poppins',
-                  color: Colors.white,
-                ),
-              ),
+            OptionContainer(text: 'Gadgets', onTap: () => navigateToDepositPage(context)),
+            OptionContainer(text: 'Shopping', onTap: () => navigateToDepositPage(context)),
+            OptionContainer(text: 'Tuition', onTap: () => navigateToDepositPage(context)),
+            OptionContainer(text: 'Starting A Business', onTap: () => navigateToDepositPage(context)),
+            OptionContainer(text: 'Others', onTap: () => showOthersDialog(context)),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void navigateToDepositPage(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const DepositPage()),
+    );
+  }
+
+  void showOthersDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        TextEditingController customController = TextEditingController();
+        return AlertDialog(
+          title: const Text('Specify'),
+          content: TextField(
+            controller: customController,
+            decoration: const InputDecoration(hintText: "Type here"),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Submit'),
+              onPressed: () {
+                // Handle the submit action here
+                Navigator.of(context).pop();
+              },
             ),
-            Container(
-              margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-              padding: const EdgeInsets.all(20.0),
-              decoration: BoxDecoration(
-                color: Colors.blueGrey,
-                borderRadius: BorderRadius.circular(10.0),
-                boxShadow: const [
-                  BoxShadow(
-                    color: Colors.black26,
-                    offset: Offset(0, 4),
-                    blurRadius: 5.0,
-                  ),
-                ],
-              ),
-              child: const Text(
-                'Shopping',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  fontFamily: 'Poppins',
-                  color: Colors.white,
-                ),
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-              padding: const EdgeInsets.all(20.0),
-              decoration: BoxDecoration(
-                color: Colors.blueGrey,
-                borderRadius: BorderRadius.circular(10.0),
-                boxShadow: const [
-                  BoxShadow(
-                    color: Colors.black26,
-                    offset: Offset(0, 4),
-                    blurRadius: 5.0,
-                  ),
-                ],
-              ),
-              child: const Text(
-                'Tuiton',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  fontFamily: 'Poppins',
-                  color: Colors.white,
-                ),
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-              padding: const EdgeInsets.all(20.0),
-              decoration: BoxDecoration(
-                color: Colors.blueGrey,
-                borderRadius: BorderRadius.circular(10.0),
-                boxShadow: const [
-                  BoxShadow(
-                    color: Colors.black26,
-                    offset: Offset(0, 4),
-                    blurRadius: 5.0,
-                  ),
-                ],
-              ),
-              child: const Text(
-                'Starting A Business',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  fontFamily: 'Poppins',
-                  color: Colors.white,
-                ),
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-              padding: const EdgeInsets.all(20.0),
-              decoration: BoxDecoration(
-                color: Colors.blueGrey,
-                borderRadius: BorderRadius.circular(10.0),
-                boxShadow: const [
-                  BoxShadow(
-                    color: Colors.black26,
-                    offset: Offset(0, 4),
-                    blurRadius: 5.0,
-                  ),
-                ],
-              ),
-              child: const Text(
-                'Others',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  fontFamily: 'Poppins',
-                  color: Colors.white,
-                ),
-              ),
+          ],
+        );
+      },
+    );
+  }
+}
+
+class OptionContainer extends StatelessWidget {
+  final String text;
+  final VoidCallback onTap;
+
+  const OptionContainer({required this.text, required this.onTap, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+        padding: const EdgeInsets.all(20.0),
+        decoration: BoxDecoration(
+          color: Colors.blueGrey,
+          borderRadius: BorderRadius.circular(10.0),
+          boxShadow: const [
+            BoxShadow(
+              color: Colors.black26,
+              offset: Offset(0, 4),
+              blurRadius: 5.0,
             ),
           ],
         ),
+        child: Text(
+          text,
+          style: const TextStyle(
+            fontSize: 20.0,
+            fontFamily: 'Poppins',
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class DepositPage extends StatelessWidget {
+  const DepositPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Deposit Page'),
+      ),
+      body: const Center(
+        child: Text('Welcome to the Deposit Page!'),
       ),
     );
   }
