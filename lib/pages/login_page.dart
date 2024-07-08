7364962468418fab9429e12d8db2ec1a4df28f9e
sebunyaronaldo <sebunyaronaldoo@gmail.com>
2024-07-08 17:04:56 +0300

    lala

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:growgrail/pages/targetpage.dart';
+import 'package:growgrail/components/my_textfield.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  LoginPage({super.key}); // Removed 'const' keyword
+
+  // Text editing controllers
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -11,107 +15,65 @@ class LoginPage extends StatelessWidget {
       body: SafeArea(
         child: Center(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              const SizedBox(height: 50),
               // logo
               const Icon(
                 Icons.lock,
                 size: 100,
               ),
-              const SizedBox(height: 20), // spacing
-              // welcome text
-              const Text(
-                'Welcome to Financial Freedom, we have missed you',
+              const SizedBox(height: 50),
+
+              // welcome to Financial freedom, we have missed you
+              Text(
+                'Welcome Back you\'ve been missed!',
                 style: TextStyle(
-                  fontSize: 20,
+                  color: Colors.grey[700],
+                  fontSize: 16,
                 ),
-                textAlign: TextAlign.center,
               ),
-              const SizedBox(height: 20), // spacing
+              const SizedBox(height: 25),
+
               // username textfield
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 25.0),
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Username',
-                  ),
-                ),
+              MyTextfield(
+                controller: usernameController,
+                hintText: 'Username',
+                obscureText: false,
               ),
-              const SizedBox(height: 10), // spacing
+
+              const SizedBox(height: 10),
+
               // password textfield
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 25.0),
-                child: TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Password',
-                  ),
+              MyTextfield(
+                controller: passwordController,
+                hintText: "Strong Password",
+                obscureText: true,
+              ),
+              // forgot password
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      'Forgot Password ?',
+                      style: TextStyle(color: Colors.grey[600]),
+                    ),
+                  ],
                 ),
               ),
-              const SizedBox(height: 20),
-               // spacing
+
+              const SizedBox(height: 25),
+
               // sign in button
-              
-              
-               ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const TargetPage()),
-                  );
-                },
-                child: const Text('Sign In'),
-              ),
-              const SizedBox(height: 20), // spacing
               // or continue with
-              const Text('Or continue with'),
-              const SizedBox(height: 10), // spacing
-              // google + apple sign in buttons
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  // google button
-                  IconButton(
-                    icon: const Icon(Icons.account_circle),
-                    onPressed: () {
-                      // google sign in logic
-                    },
-                  ),
-                  // spacing between buttons
-                  const SizedBox(width: 10),
-                  // apple button
-                  IconButton(
-                    icon: const Icon(Icons.account_circle),
-                    onPressed: () {
-                      // Apple sign in logic
-                    },
-                  ),
-                ],
-              ),
-              const SizedBox(height: 20), // spacing
-              // not a member? register
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text('Not a member?'),
-                  TextButton(
-                    onPressed: () {
-                      // registration logic
-                    },
-                    child: const Text('Register now'),
-                  ),
-                ],
-              ),
+              // google sign in + apple sign in button
+
+              // not a member
             ],
           ),
         ),
       ),
     );
   }
-}
-
-class HomePage {
-  const HomePage();
 }
