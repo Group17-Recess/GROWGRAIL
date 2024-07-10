83ffc889bdcf1ab7330aced04ceb73191356e583
QueenEmilyT <66115728+QueenEmilyT@users.noreply.github.com>
2024-07-10 16:49:53 +0300

    updated home page

--- lib/pages/splash.dart
+++ lib/pages/splash.dart
@@ -1,58 +1,53 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
-import 'home.dart';
+
+import 'package:growgrail/pages/home.dart';
 
 class SplashScreen extends StatefulWidget {
   @override
   _SplashScreenState createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin {
-  AnimationController? _controller;
-  Animation<double>? _animation;
-
+class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-
-    _controller = AnimationController(
-      duration: const Duration(seconds: 3),
-      vsync: this,
-    )..forward();
-
-    _animation = CurvedAnimation(
-      parent: _controller!,
-      curve: Curves.easeIn,
-    );
-
-    Timer(Duration(seconds: 4), () {
-      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_) => MyHomePage(title: 'GrowGrail')));
+    // Simulate a delay before navigating to the home screen
+    Timer(Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => MyHomePage(title: 'GrowGrail')),
+      );
     });
-  }
-
-  @override
-  void dispose() {
-    _controller?.dispose();
-    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.amber, // Set the background color to gold
+      backgroundColor: Colors.blue, // Example background color
       body: Center(
-        child: FadeTransition(
-          opacity: _animation!,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset('assets/images/money.jpg', width: 100.0, height: 100.0),
-              SizedBox(height: 20.0),
-            ],
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset(
+              'images/GGlogo.png', // Replace with your logo asset path
+              width: 100, // Adjust size as needed
+              height: 100,
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Welcome to GrowGrail!',
+              style: TextStyle(
+                fontSize: 24,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10),
+            CircularProgressIndicator(), // Example loading indicator
+          ],
         ),
       ),
     );
   }
 }
-
