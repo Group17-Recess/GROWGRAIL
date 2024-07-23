a7da01ce355396dc516e20b7c0ce5a87acc4d84c
sebunyaronaldo <sebunyaronaldoo@gmail.com>
2024-07-23 14:07:30 +0300

    opps

--- lib/pages/paymentservice.dart
+++ lib/pages/paymentservice.dart
@@ -4,6 +4,10 @@ import 'package:flutterwave_standard/models/requests/customer.dart';
 import 'package:flutterwave_standard/models/responses/charge_response.dart';
 
 class PaymentService {
+  final String publicKey = 'FLWPUBK-3e4514215898208e91495577492530d7-X';
+  final String secretKey =
+      'FLWSECK-2a14c6ee2ffa2246fa4974adb05cc4c2-190a8801ecbvt-X';
+
   Future<bool> initiatePayment({
     required BuildContext context,
     required String amount,
@@ -27,6 +31,7 @@ class PaymentService {
       paymentOptions: "mobilemoneyuganda",
       customization: Customization(title: "Test Payment"),
       txRef: txRef,
+      isTestMode: false, // Change to false for live payments
       redirectUrl:
           "https://your-redirect-url.com", // You can use any URL for testing
     );
