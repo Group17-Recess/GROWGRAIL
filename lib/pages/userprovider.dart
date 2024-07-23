07316340cc8fa582a63ec6fdeb1b7f7116866263
Atuhaire Brian <bryanatuhaire3@gmail.com>
2024-07-23 11:50:03 +0300

    added goal cards

--- lib/pages/userprovider.dart
+++ lib/pages/userprovider.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import '../models/goal.dart';
 
 class UserProvider with ChangeNotifier {
   double _targetAmount = 0;
   double _totalSavings = 0;
   String _name = '';
-  String _phoneNumber = ''; // Add a field for the user's phone number
+  String _phoneNumber = '';
+  List<Goal> _goals = [];
 
   double get targetAmount => _targetAmount;
   double get totalSavings => _totalSavings;
-  String get name => _name; // Add a getter for the name
-  String get phoneNumber => _phoneNumber; // Add a getter for the phone number
+  String get name => _name;
+  String get phoneNumber => _phoneNumber;
+  List<Goal> get goals => _goals;
 
   // Method to set user details
   Future<void> setUser(String name, String phone) async {
@@ -22,12 +25,14 @@ class UserProvider with ChangeNotifier {
 
     if (querySnapshot.docs.isNotEmpty) {
       _name = name;
-      _phoneNumber = phone; // Set the phone number
+      _phoneNumber = phone;
+      await fetchGoals(); // Fetch user goals after setting user details
     } else {
       _name = '';
-      _phoneNumber = ''; // Reset the phone number if not found
+      _phoneNumber = '';
+      _goals = []; // Reset goals if user not found
     }
-    notifyListeners(); // Notify listeners to update UI
+    notifyListeners();
   }
 
   void setTargetAmount(double amount) {
@@ -38,5 +43,26 @@ class UserProvider with ChangeNotifier {
   void addSavings(double amount) {
     _totalSavings += amount;
     notifyListeners();
+  }
+
+  // Fetch user goals
+  Future<void> fetchGoals() async {
+    final docSnapshot = await FirebaseFirestore.instance
+        .collection('Goals')
+        .doc(_phoneNumber)
+        .get();
+
+    if (docSnapshot.exists) {
+      final goalData = docSnapshot.data()!;
+      _goals = [Goal.fromJson(goalData)];
+    } else {
+      _goals = [];
+    }
+    notifyListeners();
+  }
+
+  // Check if the user has goals
+  bool hasGoals() {
+    return _goals.isNotEmpty;
   }
 }
